7135a9a691211118d09d054c07ccf1a9c138396a
nima minaei <s.nima.min.t@gmail.com>
2024-04-23 20:07:15 +0330

    Build a Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@
 function App() {
   return( 
   <Grid templateAreas={{
-    base: `"nav" main"`,
-    lg: `"nav nav" "aside main"`
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"`,
   }}>
     <GridItem area="nav" bg="coral">Nav</GridItem>
-    <Show above="lg" >
+    <Show above="lg">
       <GridItem area="aside" bg="gold">Aside</GridItem>
     </Show>
     <GridItem area="main" bg="dodgerblue">Main</GridItem>
   </Grid>
-  )
+  );  
 }
 
 
