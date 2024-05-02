8033eb617794155a6cae02b78a5d29481f827f18
nima minaei <s.nima.min.t@gmail.com>
2024-05-03 03:03:26 +0330

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,7 +23,8 @@ interface Address {
 const useGames = (gameQuery: GameQuery) => useData<Game>('/users', {params: {
         genres:gameQuery.genre?.id,
         platforms:gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
     }}
     , [gameQuery])
 //     {
